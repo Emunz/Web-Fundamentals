a75784bc34f0796e201820ae295bd2825b2098df
Evan Munz <emunz14@gmail.com>
2017-09-25 09:11:18 -0500

    adding For A Few Billion exercise

---
+++ JavaScript/ForAFewBillion/script.js
@@ -0,0 +1,45 @@
+var moneyDoubler = 0.01;
+
+for(var i = 1; i < 31; i++) {
+    moneyDoubler = moneyDoubler * 2;
+}
+
+console.log(moneyDoubler)
+// $10737418.24 in 30 days
+
+
+
+
+var moneyDoubler = 0.01;
+
+for(var i = 1; i < 21; i++) {
+    moneyDoubler = moneyDoubler * 2;
+}
+
+console.log(moneyDoubler)
+// will go over $10,000 in 20 days
+
+
+
+
+var moneyDoubler = 0.01;
+
+for(var i = 1; i < 38; i++) {
+    moneyDoubler = moneyDoubler * 2;
+}
+
+console.log(moneyDoubler)
+// will go over 1 billion in 37 days
+
+
+
+
+
+var moneyDoubler = 0.01;
+
+for(var i = 1; i < 1032; i++) {
+    moneyDoubler = moneyDoubler * 2;
+}
+
+console.log(moneyDoubler)
+// will go to infinity in 1031 days
